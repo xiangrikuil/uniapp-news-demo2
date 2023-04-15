c14e3c243b5771b8ee675bea92829132dd399e59
不要默认的人生 <2433646470@qq.com>
2023-04-15 21:36:18 +0800

    first commit

---
+++ unpackage/dist/build/mp-weixin/components/newsbox/newsbox.js
@@ -0,0 +1,10 @@
+(global["webpackJsonp"]=global["webpackJsonp"]||[]).push([["components/newsbox/newsbox"],{8517:function(t,n,e){"use strict";e.r(n);var u=e("ba0f"),c=e("8b57");for(var i in c)["default"].indexOf(i)<0&&function(t){e.d(n,t,(function(){return c[t]}))}(i);e("c0e0");var r=e("f0c5"),a=Object(r["a"])(c["default"],u["b"],u["c"],!1,null,null,null,!1,u["a"],void 0);n["default"]=a.exports},"8b57":function(t,n,e){"use strict";e.r(n);var u=e("9960"),c=e.n(u);for(var i in u)["default"].indexOf(i)<0&&function(t){e.d(n,t,(function(){return u[t]}))}(i);n["default"]=c.a},9960:function(t,n,e){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),n.default=void 0;var u={name:"newsbox",props:{item:{type:Object,default:function(){return{title:"组件内默认的标题",author:"张三",hits:668,picurl:"../../static/images/nopic.jpg"}}}},data:function(){return{}}};n.default=u},"9c60":function(t,n,e){},ba0f:function(t,n,e){"use strict";e.d(n,"b",(function(){return u})),e.d(n,"c",(function(){return c})),e.d(n,"a",(function(){}));var u=function(){var t=this.$createElement;this._self._c},c=[]},c0e0:function(t,n,e){"use strict";var u=e("9c60"),c=e.n(u);c.a}}]);
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'components/newsbox/newsbox-create-component',
+    {
+        'components/newsbox/newsbox-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('543d')['createComponent'](__webpack_require__("8517"))
+        })
+    },
+    [['components/newsbox/newsbox-create-component']]
+]);
